7c9d049ac0a2cd66fdb9b158874eb3bcd600e2c1
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-02-01 17:09:43 +0100

    Add Stream to read firebase

--- lib/app/features/bottomnavigationbar/newtrainingday/newtrainingday_page.dart
+++ lib/app/features/bottomnavigationbar/newtrainingday/newtrainingday_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gymapp/app/features/bottomnavigationbar/newtrainingday/newexercise/newexercise_page.dart';
 
@@ -34,37 +36,137 @@ class NewTrainingDay extends StatelessWidget {
               ),
             );
           }),
-      body: Container(
-        color: Colors.amber,
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+      body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('exerciseday').snapshots(),
+          builder: (context, snapshot) {
+            final documents = snapshot.data!.docs;
+            return Container(
+              color: Colors.amber,
+              child: ListView(
                 children: [
-                  TextField(
-                      style: TextStyle(color: Colors.amber),
-                      cursorColor: Colors.amber,
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.title,
-                            color: Colors.amber,
-                          ),
-                          hintText: 'Day name',
-                          filled: true,
-                          fillColor: Colors.grey[700],
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.amber)))),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextField(
+                            style: TextStyle(color: Colors.amber),
+                            cursorColor: Colors.amber,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.title,
+                                  color: Colors.amber,
+                                ),
+                                hintText: 'Day name',
+                                filled: true,
+                                fillColor: Colors.grey[700],
+                                border: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.amber)))),
+                        for (final document in documents) ...[
+                          Container(
+                              padding: EdgeInsets.all(20),
+                              color: Colors.grey[700],
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text('Comment:  ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                      Text(document['comment'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('Exercise name:  ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                      Text(document['exercisename'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('Musclepart:  ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                      Text(document['muslepart'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('Reps:  ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                      Text(document['reps'].toString(),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('Sets:  ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                      Text(document['sets'].toString(),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('Weight:  ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                      Text(document['weight'].toString(),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.amber)),
+                                    ],
+                                  ),
+                                ],
+                              )),
+                        ]
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  )
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
